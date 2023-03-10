3559d1418fd941be59800ce0cf4ad8656b6780e0
akil-tajwar <akiltajwar@gmail.com>
2023-03-10 16:31:16 +0600

    data loading time bug fixed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const loadAi = async () => {
 const displayAi = (datas) => {
     const cardContainer = document.getElementById('card-container');
     datas.tools.forEach(data => {
-        console.log(data);
+        // console.log(data);
         const cardDiv = document.createElement('div');
         cardDiv.innerHTML = `
         <div class="card card-compact w-auto h-full bg-base-100 border-gray-200 border-2">
@@ -34,8 +34,8 @@ const displayAi = (datas) => {
                         </div>
                     </div>
                     <div class="my-auto">
-                        <label for="my-modal-5" class="btn hover:bg-[#ffdddd] bg-white border-none">
-                            <i onclick="aiDetails('${data.id}')" class="fa-solid fa-arrow-right text-[#EB5757]"></i>
+                        <label onclick="aiDetails('${data.id}')" for="my-modal-5" class="btn hover:bg-[#ffdddd] bg-white border-none">
+                            <i class="fa-solid fa-arrow-right text-[#EB5757]"></i>
                         </label>
                     </div>  
                 </div>
@@ -45,7 +45,6 @@ const displayAi = (datas) => {
         cardContainer.appendChild(cardDiv);
     });
 }
-loadAi();
 
 const aiDetails = async (id) => {
     const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
@@ -61,9 +60,60 @@ const aiDetails = async (id) => {
 }
 const modalDetails = (info) => {
     console.log(info);
-    document.getElementById('modal-info').innerHTML = `
-        <h1 class="text-xl font-semibold pb-2">${info.description}</h1>
-        <figure><img src="${info.image_link[0]}" alt="" /></figure>
+    document.getElementById('modal-info').innerHTML = " ";
+    const mainDiv = document.createElement('div')
+    mainDiv.innerHTML = `
+        <div class="flex gap-5">
+            <div class="bg-[#ffdddd] rounded-lg p-8 border-[#EB5757] border-2">
+                <h1 class="text-xl font-bold pb-2">${info.description}</h1>
+                <div class="flex gap-4 py-6">
+                    <div class="bg-white p-6 font-semibold text-center rounded-lg text-[#03A30A]">
+                        <div class="my-auto h-full">
+                            <p>${info.pricing ? info.pricing[0].price : "no data"}</p>
+                            <p>${info.pricing ? info.pricing[0].plan : "no data"}</p>
+                        </div>
+                    </div>
+                    <div class="bg-white p-6 font-semibold text-center rounded-lg text-[#F28927]">
+                        <div class="my-auto h-full">
+                            <p>${info.pricing ? info.pricing[1].price : "no data"}</p>
+                            <p>${info.pricing ? info.pricing[1].plan : "no data"}</p>
+                        </div>
+                    </div>
+                    <div class="bg-white p-6 font-semibold text-center rounded-lg text-[#EB5757]">
+                        <div class="my-auto h-full">
+                            <p>${info.pricing ? info.pricing[2].price : "no data"}</p>
+                            <p>${info.pricing ? info.pricing[2].plan : "no data"}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex justify-between gap-8">
+                    <div>
+                        <h1 class="text-xl font-bold pb-2">Features</h1>
+                        <ul class="list-disc ml-4">
+                            <li>${info.features ? info.features[1].feature_name : "no data"}</li>
+                            <li>${info.features ? info.features[2].feature_name : "no data"}</li>
+                            <li>${info.features ? info.features[3].feature_name : "no data"}</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h1 class="text-xl font-bold pb-2">Integration</h1>
+                        <ul class="list-disc ml-4">
+                            <li>${info.integrations ? info.integrations[0] : "no data"}</li>
+                            <li>${info.integrations ? info.integrations[1] : "no data"}</li>
+                            <li>${info.integrations ? info.integrations[2] : "no data"}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="rounded-lg p-8 border-gray-200 border-2">
+                <figure><img class="rounded-lg" src="${info.image_link ? info.image_link[0] : "no data"}" alt="" /></figure>
+                <div class="py-8 text-center">
+                    <h1 class="text-xl font-bold pb-2">${info.input_output_examples ? info.input_output_examples[0].input : "no data"}</h1>
+                    <p>${info.input_output_examples ? info.input_output_examples[0].output : "no data"}</p>
+                </div>
+            </div>
+        </div>
         `;
+        document.getElementById('modal-info').appendChild(mainDiv);
 }
-aiDetails();
+loadAi();
